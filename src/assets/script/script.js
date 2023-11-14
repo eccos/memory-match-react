a417b89f741f8298c0a2090e3b75a6304258130b
eccos <eccos@users.noreply.github.com>
2023-11-14 13:51:47 -0800

    cleanup react converted js

--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -1,20 +1,8 @@
-let card1 = null;
-let card2 = null;
-let cardMatchCount = 0;
 let halfGridSize = 0;
-let isLockedBoard = false;
 // TODO: implement time entry/selection for custom countdown
 // TODO: implement lose conditions/challenges for timers
 let countdownIntervalId = null;
 let timerIntervalId = null;
-
-let moveCount = 0;
-const moveCountSpan = document.querySelector("#move-count");
-
-const selectGridSize = document.querySelector("#select-grid-size");
-const btnStartGame = document.querySelector("#btn-start-game");
-const cardGrid = document.querySelector("#card-grid");
-btnStartGame.addEventListener("click", startGame);
 
 const btnOptions = document.getElementById("btn-options");
 const optionsPanel = document.getElementById("options-panel");
