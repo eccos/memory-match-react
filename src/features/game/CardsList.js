e38934a55bc1589bcd2cd963cd85d30f45a5f3ce
eccos <eccos@users.noreply.github.com>
2023-11-14 13:42:15 -0800

    add turn counter & win message by using state

--- src/features/game/CardsList.js
+++ src/features/game/CardsList.js
@@ -2,6 +2,47 @@ import Card from "./Card";
 import { Container } from "reactstrap";
 import { useEffect, useState } from "react";
 
+  // function createCustomGrid(rowCardLen, colCardLen) {
+  //   const gridSize = rowCardLen * colCardLen;
+  //   const rowCardLimit = 12;
+  //   const colCardLimit = 6;
+  //   if (rowCardLen > rowCardLimit) {
+  //     console.error(`Row can only have ${rowCardLimit} cards max`);
+  //     return;
+  //   }
+  //   if (colCardLen > colCardLimit) {
+  //     console.error(`Column can only have ${colCardLimit} cards max`);
+  //     return;
+  //   }
+  //   if (gridSize % 2 !== 0) {
+  //     console.error("Grid cannot have an odd number of cards");
+  //     return;
+  //   }
+  //   const uniqueCards = createUniqueCards(gridSize);
+  //   const pairCards = uniqueCards.concat(uniqueCards);
+  //   const cards = shuffle(pairCards);
+
+  //   const rows = [];
+  //   let cardIndex = 0;
+  //   for (let row = 0; row < colCardLen; row++) {
+  //     // create html row
+  //     const row = createDomRow();
+  //     for (let col = 0; col < rowCardLen; col++) {
+  //       // create html col & create/append card
+  //       const col = createDomCol();
+  //       const cardId = "card-" + (cardIndex + 1);
+  //       const card = createDomFacedownCard(cardId, cards[cardIndex]);
+  //       const faceupCard = createDomFaceupCard(cardId, cards[cardIndex]);
+  //       cardIndex++;
+  //       col.appendChild(card);
+  //       col.appendChild(faceupCard);
+  //       row.appendChild(col);
+  //     }
+  //     rows.push(row);
+  //   }
+  //   return rows;
+  // }
+
 const rowCount = 2;
 const colCount = 3;
 const gridSize = rowCount * colCount;
@@ -53,7 +94,8 @@ const CardsList = () => {
   }
 
   // function startGame() {
-  //   resetGame();
+  //   halfGridSize = 0;
+  //   stopTimers();
   //   const [x, y] = selectGridSize.value.split("x");
   //   halfGridSize = (x * y) / 2;
   //   const gridRows = createCustomGrid(x, y);
@@ -62,21 +104,6 @@ const CardsList = () => {
   //   });
   //   countdownIntervalId = startCountdown();
   //   timerIntervalId = startTimer();
-  // }
-
-  // function resetGame() {
-  //   card1 = null;
-  //   card2 = null;
-  //   cardMatchCount = 0;
-  //   halfGridSize = 0;
-  //   isLockedBoard = false;
-
-  //   moveCount = 0;
-  //   moveCountSpan.textContent = `Moves: ${moveCount}`;
-
-  //   stopTimers();
-
-  //   cardGrid.innerHTML = null;
   // }
 
   function checkCards() {
@@ -102,17 +129,6 @@ const CardsList = () => {
     setIsLockedBoard(false);
   }
 
-  // function checkWinCondition() {
-  //   if (cardMatchCount < halfGridSize - 1) {
-  //     return false;
-  //   }
-  //   // win early if only 2 cards remain
-  //   stopTimers();
-  //   cardGrid.innerHTML = null;
-  //   cardGrid.textContent = `YOU WIN! It took you ${moveCount} moves.`;
-  //   return true;
-  // }
-
   function handleClick(card) {
     if (isLockedBoard) return;
     !choice1 ? setChoice1(card) : setChoice2(card);
@@ -123,15 +139,16 @@ const CardsList = () => {
     if (!choice2) return;
     checkCards();
     setTurns((prevTurns) => prevTurns + 1);
-    // checkWinCondition();
+    // if winner, stopTimers();
   }, [choice2]);
-
-  console.table(cards);
 
   return (
     <Container>
       <button onClick={startGame}>New Game</button>
-
+      {cards.length > 0 && <p>Turn: {turns}</p>}
+      {!cards.find((card) => !card.matched) && (
+        <p>YOU WIN! It took you {turns} moves.</p>
+      )}
       <div className="card-grid">
         {cards.map((card) => (
           <Card
@@ -144,47 +161,6 @@ const CardsList = () => {
       </div>
     </Container>
   );
-
-  // function createCustomGrid(rowCardLen, colCardLen) {
-  //   const gridSize = rowCardLen * colCardLen;
-  //   const rowCardLimit = 12;
-  //   const colCardLimit = 6;
-  //   if (rowCardLen > rowCardLimit) {
-  //     console.error(`Row can only have ${rowCardLimit} cards max`);
-  //     return;
-  //   }
-  //   if (colCardLen > colCardLimit) {
-  //     console.error(`Column can only have ${colCardLimit} cards max`);
-  //     return;
-  //   }
-  //   if (gridSize % 2 !== 0) {
-  //     console.error("Grid cannot have an odd number of cards");
-  //     return;
-  //   }
-  //   const uniqueCards = createUniqueCards(gridSize);
-  //   const pairCards = uniqueCards.concat(uniqueCards);
-  //   const cards = shuffle(pairCards);
-
-  //   const rows = [];
-  //   let cardIndex = 0;
-  //   for (let row = 0; row < colCardLen; row++) {
-  //     // create html row
-  //     const row = createDomRow();
-  //     for (let col = 0; col < rowCardLen; col++) {
-  //       // create html col & create/append card
-  //       const col = createDomCol();
-  //       const cardId = "card-" + (cardIndex + 1);
-  //       const card = createDomFacedownCard(cardId, cards[cardIndex]);
-  //       const faceupCard = createDomFaceupCard(cardId, cards[cardIndex]);
-  //       cardIndex++;
-  //       col.appendChild(card);
-  //       col.appendChild(faceupCard);
-  //       row.appendChild(col);
-  //     }
-  //     rows.push(row);
-  //   }
-  //   return rows;
-  // }
 };
 
 export default CardsList;
