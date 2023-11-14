b460d59e6433ed4ac33b035da4429bc129e6635b
eccos <eccos@users.noreply.github.com>
2023-11-14 00:00:34 -0800

    move functions to CardsList

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,32 +2,6 @@ import Grid from "../features/game/Grid";
 
 const Game = () => {
   return <Grid />;
-  // function startGame() {
-  //   resetGame();
-  //   const [x, y] = selectGridSize.value.split("x");
-  //   halfGridSize = (x * y) / 2;
-  //   const gridRows = createCustomGrid(x, y);
-  //   gridRows.forEach((row) => {
-  //     cardGrid.appendChild(row);
-  //   });
-  //   countdownIntervalId = startCountdown();
-  //   timerIntervalId = startTimer();
-  // }
-
-  // function resetGame() {
-  //   card1 = null;
-  //   card2 = null;
-  //   cardMatchCount = 0;
-  //   halfGridSize = 0;
-  //   isLockedBoard = false;
-
-  //   moveCount = 0;
-  //   moveCountSpan.textContent = `Moves: ${moveCount}`;
-
-  //   stopTimers();
-
-  //   cardGrid.innerHTML = null;
-  // }
 };
 
 export default Game;
